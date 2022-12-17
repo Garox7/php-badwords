cf32eec649da012a2675f6851dd6cc520dd4b378
Garox7 <110902920+Garox7@users.noreply.github.com>
2022-12-17 17:46:04 +0100

    Fix

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <body>
     <?php
         $paragraph = 'Il web developer è una figura chiave in azienda: lo sviluppatore web infatti si occupa di costruire siti e applicazioni web nella loro interezza, creando l\'interfaccia che vede l\'utente e tutti i meccanismi invisibili che permettono al sistema di funsionare. Il metodo Boolean ti permetterà di farlo con un mix di teoria e pratica, attraverso esercizi che replicano le richieste del mondo del lavoro e con un programma sempre aggiornato studiato da sviluppatori professionisti.';
-        $badword = trim($_GET['cancelled_word']);
+        $badword = trim($_GET['cancelled_word'] ?? '');
         $replace = '***';
         $new_paragraph = str_ireplace($badword, $replace, $paragraph);
         $new_string_len = strlen($new_paragraph);
